a4dd13cfa51e2aa090f0865fc91e9b05870b4850
raulsg13 <rsg1018@alu.ubu.es>
2025-04-22 16:46:45 +0200

    Comentarios

--- src/lsi/ubu/servicios/ServicioImpl.java
+++ src/lsi/ubu/servicios/ServicioImpl.java
@@ -66,13 +66,15 @@ public class ServicioImpl implements Servicio {
 		try {
 			con = pool.getConnection();
 			
-			//Comprobaciones previas
+			//Comprobaciones previas vehiculo
 			try {
-				inexMatricula= con.prepareStatement("SELECT matricula FROM vehiculos WHERE matricula = ?");
+				//Obtener vehiculo con la matricula indicada
+				inexMatricula= con.prepareStatement("SELECT * FROM vehiculos WHERE matricula = ?");
 				inexMatricula.setString(1, matricula);
 				
 				rsMatricula = inexMatricula.executeQuery();
 				
+				//Si no existe, lanzar excepcion
 				if(!rsMatricula.next()) {
 					throw new AlquilerCochesException(AlquilerCochesException.VEHICULO_NO_EXIST);
 				}	
@@ -86,13 +88,15 @@ public class ServicioImpl implements Servicio {
 				}
 			}
 			
-			//Comprobaciones previas
+			//Comprobaciones previas cliente
 			try {
+				//Obtener cliente con el NIF indicado
 				inexCliente = con.prepareStatement("SELECT * FROM clientes WHERE NIF = ?");
 				inexCliente.setString(1, nifCliente);
 				
 				rsCliente = inexCliente.executeQuery();
 				
+				//Si no existe, lanzar excepcion
 				if(!rsCliente.next()) {
 					throw new AlquilerCochesException(AlquilerCochesException.CLIENTE_NO_EXIST);
 				}	
@@ -108,18 +112,21 @@ public class ServicioImpl implements Servicio {
 			
 			//Comprobaciones previas coche no disponible
 			try {
-				
+				//Obtenemos las fechas de inicio y fin de reservas apuntadas del coche indicado
 				dispVehiculo= con.prepareStatement("SELECT fecha_ini, fecha_fin FROM reservas WHERE matricula = ?");
 				dispVehiculo.setString(1, matricula);
 				
-				
 				rsVehiculo = dispVehiculo.executeQuery();
 
+				//De cada reserva de dicho vehiculo...
 				while (rsVehiculo.next()) {
 					
 					java.sql.Date ultimaResFechaFin=rsVehiculo.getDate("fecha_fin");
 					java.sql.Date ultimaResFechaIni=rsVehiculo.getDate("fecha_ini");
-				
+					
+					//Si la fecha de inicio de la reserva a realizar no es posterior al fin de otra
+					// y la fecha de fin no es anterior al inicio,
+					// se lanza la excepcion
 					if (!(sqlFechaIni.after(ultimaResFechaFin)  || sqlFechaFin.before(ultimaResFechaIni))) {
 						throw new AlquilerCochesException(AlquilerCochesException.VEHICULO_OCUPADO);
 					}
@@ -134,7 +141,7 @@ public class ServicioImpl implements Servicio {
 				}
 			}
 			
-			
+			//Tras las comprobaciones, insertamos la nueva reserva
 			st = con.prepareStatement("INSERT INTO reservas (idReserva, Cliente, matricula, fecha_ini, fecha_fin) "
 					+ "VALUES(seq_reservas.nextval, ?, ?, ?, ?)");
 			st.setString(1, nifCliente);
@@ -144,6 +151,7 @@ public class ServicioImpl implements Servicio {
 
 			st.executeUpdate();
 			
+			//Obtenemos datos necesarios para calcular la factura
 			String query = "SELECT precio_cada_dia, capacidad_deposito, precio_por_litro, id_modelo, tipo_combustible "
 					+ "FROM vehiculos "
 					+ "NATURAL JOIN modelos "
@@ -157,32 +165,40 @@ public class ServicioImpl implements Servicio {
 			
 			rs.next();
 			
+			//Calculamos el precio del alquiler multiplicando el precio por dia por los dias alquilados
 			BigDecimal dias = new BigDecimal(diasDiff);
 			BigDecimal precio_dias = rs.getBigDecimal(1).multiply(dias);
 			
+			//Calculamos el precio del combustible multiplicando la capacidad del deposito por el precio por litro
 			BigDecimal capacidad = new BigDecimal(rs.getInt(2));
 			BigDecimal precio_combustible = rs.getBigDecimal(3).multiply(capacidad);
+			
+			//Calculamos el precio total de la reserva sumando el precio por combusitble con el precio por alquiler
 			BigDecimal precio_total = precio_dias.add(precio_combustible);
 			
+			//Insertamos la factura total del alquiler
 			st = con.prepareStatement("INSERT INTO facturas VALUES (seq_num_fact.nextval, ?, ?)");
 			st.setBigDecimal(1, precio_total);
 			st.setString(2, nifCliente);
 			
 			st.executeUpdate();
 			
+			//Insertamos la linea de factura por dias de alquiler
 			st = con.prepareStatement("INSERT INTO lineas_factura VALUES (seq_num_fact.currval, ?, ?)");
 			String concepto = dias + " dias de alquiler, vehiculo modelo " + rs.getInt(4);
 			st.setString(1, concepto);
 			st.setBigDecimal(2, precio_dias);
 			st.executeUpdate();
 			
+			//Insertamos la linea de factura por combustible gastado
 			st = con.prepareStatement("INSERT INTO lineas_factura VALUES (seq_num_fact.currval, ?, ?)");
 			concepto = "Deposito lleno de " + capacidad + " litros de " + rs.getString(5);
 			st.setString(1, concepto);
 			st.setBigDecimal(2, precio_combustible);
 			st.executeUpdate();
 			
-			con.commit();		
+			con.commit();
+			
 			/* A completar por el alumnado... */
 
 			/* ================================= AYUDA R�PIDA ===========================*/
